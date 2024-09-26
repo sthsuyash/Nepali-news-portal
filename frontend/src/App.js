b1cc6af1c2e2f6c6a1ffc10a01fb609bdcb3f683
Amish Bajracharya <amishbajracharya63@gmail.com>
2024-09-26 15:34:50 +0545

    App files

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import LoginSignup from "./user-auth/auth-form.js";
+
+function App() {
+  return (
+    <div className="App">
+      <LoginSignup />
+    </div>
+  );
+}
+
+export default App;
