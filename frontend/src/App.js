a545b6babe3c1778c1ec31d3787e04b2cc27685e
Amish Bajracharya <amishbajracharya63@gmail.com>
2024-09-27 17:51:41 +0545

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginSignup from "./user-auth/auth-form.js";
+import NewsCrud from "./crud-page/postings.js";
 
 function App() {
   return (
-    <div className="App">
-      <LoginSignup />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginSignup />} />
+        <Route path="/news-crud" element={<NewsCrud />} NewsCrud />
+      </Routes>
+    </Router>
   );
 }
 
