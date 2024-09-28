efd28895a63df40e7c9101856273676dd6af568d
Amish Bajracharya <amishbajracharya63@gmail.com>
2024-09-28 22:23:42 +0545

    added fontFamily in theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+      body: ["Montserrat", "sans-serif"],
+      login: ["MongoDB Value Serif", "Times New Roman", "serif"],
+    },
     extend: {},
   },
   plugins: [],
