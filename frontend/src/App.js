493e645259fd6880b91386a3048c3c8e2fc7f594
Amish Bajracharya <amishbajracharya63@gmail.com>
2024-09-29 23:47:24 +0545

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import LoginSignup from "./user-auth/auth-form.js";
-import NewsCrud from "./user-dashboard/dashboard.js";
+import LoginSignup from "./auth/auth-form.js";
+import UserDashboard from "./user-dashboard/dashboard.js";
+import AdminDashboard from "./admin-dashboard/dashboard.js";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginSignup />} />
-        <Route path="/news-crud" element={<NewsCrud />} NewsCrud />
+        <Route path="/user/dashboard" element={<UserDashboard />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
       </Routes>
     </Router>
   );
