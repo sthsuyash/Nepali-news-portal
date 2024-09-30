c15416984996a4e35d840f186ad0071f91633abb
Amish Bajracharya <amishbajracharya63@gmail.com>
2024-09-30 23:18:49 +0545

    user dashboard

--- frontend/src/auth/auth-form.js
+++ frontend/src/auth/auth-form.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FaNewspaper } from "react-icons/fa";
+import axios from "axios";
+
 const LoginSignup = () => {
   const navigate = useNavigate();
   const [isLogin, setIsLogin] = useState(true);
   const [formData, setFormData] = useState({
     username: "",
     email: "",
+    phone_number: "",
     password: "",
     confirmPassword: "",
   });
@@ -16,6 +19,7 @@ const LoginSignup = () => {
     setFormData({
       username: "",
       email: "",
+      phone_number: "",
       password: "",
       confirmPassword: "",
     });
@@ -30,19 +34,43 @@ const LoginSignup = () => {
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (isLogin) {
-      console.log("Logging Details:", formData);
-      navigate("/user/dashboard");
-    } else {
-      if (formData.password !== formData.confirmPassword) {
-        alert("Passwords don't match!");
-        return;
+    try {
+      if (isLogin) {
+        const response = await axios.post("http://127.0.0.1:8000/login", {
+          username: formData.username,
+          password: formData.password,
+        });
+
+        console.log("Login Successful:", response.data);
+
+        console.log("Login Successful:", response.data);
+        localStorage.setItem("token", response.data.access_token);
+        navigate("/user/dashboard");
+      } else {
+        // Signup API call
+        if (formData.password !== formData.confirmPassword) {
+          alert("Passwords don't match!");
+          return;
+        }
+        const response = await axios.post("http://127.0.0.1:8000/register", {
+          username: formData.username,
+          email: formData.email,
+          phone_number: formData.phone_number,
+          password: formData.password,
+        });
+
+        console.log("Signup Successful:", response.data);
+        setIsLogin(true); // Switch to login form after successful signup
       }
-      console.log("Signing Details:", formData);
-      setIsLogin(true);
+    } catch (error) {
+      console.error(
+        "Error during authentication:",
+        error.response ? error.response.data : error.message
+      );
+      alert("Invalid Username or Password");
     }
   };
 
@@ -50,97 +78,109 @@ const LoginSignup = () => {
 
   return (
     <div className="min-h-screen flex flex-col justify-center items-center backdrop-blur-sm">
-  
-  <div className="w-full max-w-2xl bg-slate-200 shadow-md rounded-2xl p-8">
-    
-    <div className="text-left flex space-x-4 mb-6">
-      <FaNewspaper className="text-teal-600 text-3xl" />
-      <h1 className="text-2xl font-bold text-teal-600">KhabarCheck</h1>
-    </div>
+      <div className="w-full max-w-2xl bg-slate-200 shadow-md rounded-2xl p-8">
+        <div className="text-left flex space-x-4 mb-6">
+          <FaNewspaper className="text-teal-600 text-3xl" />
+          <h1 className="text-2xl font-bold text-teal-600">KhabarCheck</h1>
+        </div>
 
-    <h2 className="text-3xl font-login text-left mb-6">
-      {isLogin ? "Log in to KhabarCheck" : "Create your account"}
-    </h2>
-    <form onSubmit={handleSubmit} className="space-y-6">
-      {!isLogin && (
-        <div className="input-field">
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-            className="w-full px-3 py-2 shadow-sm bg-slate-200 rounded-md focus:outline-none focus:ring-0 border-b-2 border-gray-400 focus:border-teal-500"
-            placeholder="Username"
-            required
-          />
-        </div>
-      )}
-      <div className="input-field">
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          className="w-full px-3 py-2 bg-slate-200 rounded-md shadow-sm focus:outline-none focus:ring-0 border-b-2 border-gray-400 focus:border-teal-500"
-          placeholder="Email"
-          required
-        />
-      </div>
-      <div className="input-field relative">
-        <input
-          type={showPassword ? "text" : "password"}
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          className="w-full px-3 py-2 bg-slate-200 rounded-md shadow-sm focus:outline-none focus:ring-0 border-b-2 border-gray-400 focus:border-teal-500"
-          placeholder="Password"
-          required
-        />
-        <button
-          type="button"
-          onClick={toggleShowPassword}
-          className="absolute right-3 top-2 text-gray-600"
-        >
-          {showPassword ? (
-            <i className="fas fa-eye-slash"></i>
-          ) : (
-            <i className="fas fa-eye"></i>
+        <h2 className="text-3xl font-login text-left mb-6">
+          {isLogin ? "Log in to KhabarCheck" : "Create your account"}
+        </h2>
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div className="input-field">
+            <input
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              className="w-full px-3 py-2 shadow-sm bg-slate-200 rounded-md focus:outline-none focus:ring-0 border-b-2 border-gray-400 focus:border-teal-500"
+              placeholder="Username"
+              required
+            />
+          </div>
+
+          {!isLogin && (
+            <div className="input-field">
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="w-full px-3 py-2 bg-slate-200 rounded-md shadow-sm focus:outline-none focus:ring-0 border-b-2 border-gray-400 focus:border-teal-500"
+                placeholder="Email"
+                required
+              />
+            </div>
           )}
-        </button>
+          {!isLogin && (
+            <div className="input-field">
+              <input
+                type="number"
+                name="phone_number"
+                value={formData.phone_number}
+                onChange={handleChange}
+                className="w-full px-3 py-2 shadow-sm bg-slate-200 rounded-md focus:outline-none focus:ring-0 border-b-2 border-gray-400 focus:border-teal-500"
+                placeholder="Phone Number"
+                required
+              />
+            </div>
+          )}
+          <div className="input-field relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              className="w-full px-3 py-2 bg-slate-200 rounded-md shadow-sm focus:outline-none focus:ring-0 border-b-2 border-gray-400 focus:border-teal-500"
+              placeholder="Password"
+              required
+            />
+            <button
+              type="button"
+              onClick={toggleShowPassword}
+              className="absolute right-3 top-2 text-gray-600"
+            >
+              {showPassword ? (
+                <i className="fas fa-eye-slash"></i>
+              ) : (
+                <i className="fas fa-eye"></i>
+              )}
+            </button>
+          </div>
+          {!isLogin && (
+            <div className="input-field">
+              <input
+                type="password"
+                name="confirmPassword"
+                value={formData.confirmPassword}
+                onChange={handleChange}
+                className="w-full px-3 py-2 bg-slate-200 rounded-md shadow-sm focus:outline-none focus:ring-0 border-b-2 border-gray-400 focus:border-teal-500"
+                placeholder="Confirm Password"
+                required
+              />
+            </div>
+          )}
+          <div className="flex justify-between items-center">
+            <button
+              type="button"
+              className="text-lg text-teal-700 no-underline font-login hover:underline"
+              onClick={toggleForm}
+            >
+              {isLogin
+                ? "Don't have an account? Sign Up"
+                : "Already have an account? Login"}
+            </button>
+            <button
+              type="submit"
+              className="w-32 bg-teal-600 text-white w-100 py-2 rounded-lg text-lg hover:bg-teal-700 transition duration-300 font-login"
+            >
+              {isLogin ? "Login" : "Sign Up"}
+            </button>
+          </div>
+        </form>
       </div>
-      {!isLogin && (
-        <div className="input-field">
-          <input
-            type="password"
-            name="confirmPassword"
-            value={formData.confirmPassword}
-            onChange={handleChange}
-            className="w-full px-3 py-2 bg-slate-200 rounded-md shadow-sm focus:outline-none focus:ring-0 border-b-2 border-gray-400 focus:border-teal-500"
-            placeholder="Confirm Password"
-            required
-          />
-        </div>
-      )}
-      <div className="flex justify-between items-center">
-        <button
-          type="button"
-          className="text-lg text-teal-700 no-underline font-login hover:underline"
-          onClick={toggleForm}
-        >
-          {isLogin
-            ? "Don't have an account? Sign Up"
-            : "Already have an account? Login"}
-        </button>
-        <button
-          type="submit"
-          className="w-32 bg-teal-600 text-white w-100 py-2 rounded-lg text-lg hover:bg-teal-700 transition duration-300 font-login"
-        >
-          {isLogin ? "Login" : "Sign Up"}
-        </button>
-      </div>
-    </form>
-  </div>
-</div>
+    </div>
   );
 };
 
